94b412f9caa6ec38a2f9082b463f565b46e98ed3
Punarvasu20003 <punarvasuingole72@gmail.com>
2026-01-15 14:26:21 +0530

    removed userId from dto

--- src/main/java/com/support/support_service/dto/CreateTicketRequest.java
+++ src/main/java/com/support/support_service/dto/CreateTicketRequest.java
@@ -1,13 +1,11 @@
 package com.support.support_service.dto;
 
 import com.support.support_service.model.enums.TicketPriority;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public class CreateTicketRequest {
-
-    @NotNull(message = "User ID is required")
-    private Long userId;
 
     @NotBlank(message = "Subject is required")
     private String subject;
@@ -17,10 +15,6 @@ public class CreateTicketRequest {
 
     @NotNull(message = "Priority is required")
     private TicketPriority priority;
-
-    public Long getUserId() {
-        return userId;
-    }
 
     public String getSubject() {
         return subject;
